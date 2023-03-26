ae069cdd5d225fb7e0bb407d9ccd3d88d7c35072
Richswag009 <richesmetelewawon@gmail.com>
2023-03-26 13:32:51 +0100

    remove the api due to cors policy

--- src/Components/Layouts/Navbar.jsx
+++ src/Components/Layouts/Navbar.jsx
@@ -4,9 +4,11 @@ import CartButton from "../Cart/CartButton";
 
 export default function Navbar() {
   return (
-    <nav className="mx-10 lg:mx-20 flex justify-between items-center py-5">
+    <nav className="mx-5 lg:mx-20 flex justify-between items-center py-5">
       <NavLink to="/">
-        <h1 className="text-3xl">J-<span className="text-slate-900 text-xl">Thrifts</span></h1>
+        <h1 className="text-3xl">
+          J-<span className="text-slate-900 text-xl">Thrifts</span>
+        </h1>
       </NavLink>
       <div>
         <NavLink to="/cart">
